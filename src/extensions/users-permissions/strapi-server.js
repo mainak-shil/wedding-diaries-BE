6fd76be43ee67a53fc8ad8321abd1d67da383dbd
Mainak <dev.mainakshil@gmail.com>
2023-05-21 20:14:46 +0530

    add post and user

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,31 @@
+module.exports = (plugin) => {
+  // custom controller
+  plugin.controllers.user.updateMe = async (ctx) => {
+    if (!ctx.state.user || !ctx.state.user.id) {
+      return (ctx.response.status = 401);
+    }
+
+    await strapi
+      .query("plugin::users-permissions.user")
+      .update({
+        where: { id: ctx.state.user.id },
+        data: ctx.request.body,
+      })
+      .then((res) => {
+        ctx.response.status = 200;
+      });
+  };
+
+  // custom route
+  plugin.routes["content-api"].routes.push({
+    method: "PUT",
+    path: "/user/me", //! singular as not to conflict existing routes
+    handler: "user.updateMe", // ! enable NEW** route in user settings
+    config: {
+      prefix: "",
+      policies: [],
+    },
+  });
+
+  return plugin;
+};
