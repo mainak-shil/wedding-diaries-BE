8f55f662e2e3ed1dd456b3868996bc8ceeda5fa3
mainak-shil <dev.mainakshil@gmail.com>
2023-12-15 11:39:57 +0530

    user/me

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,11 +1,31 @@
+const { sendAck } = require('../../utils/helper');
+
 module.exports = plugin => {
   plugin.controllers.user.updateMe = async ctx => {
-    // ...
+    if (!ctx.state.user || !ctx.state.user.id) {
+      return (ctx.response.status = 401);
+    }
+
+    const populate = {};
+    ctx.request.query?.populate?.map(e => (populate[e] = true));
+    const response = await strapi
+      .query('plugin::users-permissions.user')
+      .update({
+        where: { id: ctx.state.user.id },
+        data: ctx.request.body,
+        populate,
+        select: ['id', 'email', 'name', 'phone'],
+      });
+
+    sendAck({
+      ctx,
+      data: response,
+    });
   };
 
   plugin.routes['content-api'].routes.push({
     method: 'PUT',
-    path: '/user/<example>',
+    path: '/user/me',
     handler: 'user.updateMe',
     config: {
       prefix: '',
